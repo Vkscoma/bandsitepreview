6e8b71b3df67b4aebdc10e9c42e075a0da4d44f4
Vkscoma <vkscoma@gmail.com>
2022-07-07 19:29:03 -0700

    CSS + JS edits

--- script.js
+++ script.js
@@ -24,3 +24,29 @@ navLinks.forEach((link) => {
     xIcon.style.display = "none";
   });
 });
+
+// Form Script
+
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let comments = document.getElementById("comments");
+const success = document.getElementById("success");
+
+function sendEmail() {
+  fetch("https://formsubmit.co/ajax/vkscoma@gmail.com", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      comments: document.getElementById("comments").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .then((message) => (success.style.display = "block"))
+    .catch((error) => console.log(error));
+}
